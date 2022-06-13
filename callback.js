33a53f1ac59cb168861a96a48f5f6e51f17691a9
Rizwan Shaikh <100840582+rizwan992323@users.noreply.github.com>
2022-06-13 18:37:30 +0530

    Add files via upload

--- callback.js
+++ callback.js
@@ -1,39 +1,39 @@
+//How to creat call back
 
-//How to create callback
-
-const message = function() {  
-    console.log("This message is shown after 3 seconds");
+const message = function(){
+  console.log("print after 3 second");
 }
- 
-setTimeout(message, 3000);
-//.....
+setTimeout(message,3000);
+//anomymous
 
-function displayData() {
-    setTimeout(() => {
-      console.log("Hello Welcome to Asynchronous programming");
-    }, 2000);
-  }
-  displayData();
-  console.log("Welcome to Javascript");
-  console.log("Callback Functions...!!");
+setTimeout(function(){
+console.log("print after 4 seconds");
+
+},4000);
+
+//
 
 
-//What is an Anonymous Function?
-//Alternatively, we can define a function directly inside another function,  
-//instead of calling it. It will look like this:
-
-setTimeout(function() {  
-    console.log("This message is shown after 4 seconds");
-}, 4000);
 
 
-//Callback as an Arrow Function
-//If you prefer, you can also write the same callback function as an ES6 arrow function,
-// which is a newer type of function in JavaScript:
-
-setTimeout(() => { 
-    console.log("This message is shown after 3 seconds");
-}, 3000);
 
 
+function showData(name,amt){
+    alert("enter name"+ name + '\n  Enter amount'+ amt);
+}
+function getData(callback){
+    var  name= prompt("callback \n Enter ur name?");
+    var amt =  prompt("enter some amount");
+    callback(name,amt);
+}
+getData(showData);
+
+//....
+setTimeout(()=>{
+  console.log("this is adone");
+}  ,4000);
+//..
+setTimeout(function(){
+console.log("submited");
+} ,5000)
 
